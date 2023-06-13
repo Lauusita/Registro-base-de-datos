c8a8af44c590091d4adf054d08409378c949bae4
Lauusita <laurasofiaartetayepes@gmail.com>
2023-06-12 23:13:29 -0500

    FINAL UPDATE: adding style to php

--- primero.php
+++ primero.php
@@ -1,16 +1,16 @@
 <?php
 
-$nombre = $_POST['nombre'];
-$apellido= $_POST['apellido'];
-$documento= $_POST['documento'];
-$numero= $_POST['numero'];
-$nacimiento = $_POST['dia'].'/'.$_POST['mes'].'/'.$_POST['ano'];
-$tsangre = $_POST['sangre'].$_POST['signo'];
-$sexo = $_POST['sexo'];
-$direccion = $_POST['dc'].$_POST['numdc'].'#'.$_POST['num2dc'].'-'.$_POST['apt'];
-$ciudad = $_POST['ciudad'];
-$tel = $_POST['tel'];
-$correo = $_POST['correo'];
+// $nombre = $_POST['nombre'];
+// $apellido= $_POST['apellido'];
+// $documento= $_POST['documento'];
+// $numero= $_POST['numero'];
+// $nacimiento = $_POST['dia'].'/'.$_POST['mes'].'/'.$_POST['ano'];
+// $tsangre = $_POST['sangre'].$_POST['signo'];
+// $sexo = $_POST['sexo'];
+// $direccion = $_POST['dc'].$_POST['numdc'].'#'.$_POST['num2dc'].'-'.$_POST['apt'];
+// $ciudad = $_POST['ciudad'];
+// $tel = $_POST['tel'];
+// $correo = $_POST['correo'];
 
 
 echo "<br><h2> El nombre es </h2>".$nombre;
@@ -46,3 +46,110 @@ else{
 mysqli_close($connect); }//fin base de datos
 
 ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container p-2 mt-4 border border-dark-subtle rounded">
+        <div class="row">
+            <div class="col">
+                <a href="index.html" class="btn btn-info text-white fw-semibold" tabindex="-1" role="button" aria-disabled="true">←</a>
+            </div>
+        </div>
+        <div class="row">
+          <div class="col">
+              <h1 class="display-6 text-center bg-info text-white mt-2 fw-semibold rounded">BIENVENIDOS A PHP</h1>
+          </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h3>
+                    <?php
+                        echo  "Nombre: $nombre $apellido";
+                    ?>
+                </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h3>
+                    <?php
+                        echo "Documento de identidad: $documento, No°$numero";
+                    ?>
+                </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h3>
+                    <?php
+                        echo "Su fecha de nacimiento es $nacimiento";
+                    ?>
+                </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h3>
+                    <?php
+                        echo "Su tipo de sangre es $tsangre";
+                    ?>
+                </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h3>
+                    <?php
+                        echo "Su genero es $sexo";
+                    ?>
+                </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h3>
+                    <?php
+                        echo "Su direccion es $direccion";
+                    ?>
+                </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h3>
+                    <?php
+                        echo "Provienes de la ciudad de $ciudad";
+                    ?>
+                </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h3>
+                    <?php
+                        echo "Su telefono es $tel";
+                    ?>
+                </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h3>
+                    <?php
+                        echo "Y, tu correo es $correo";
+                    ?>
+                </h3>
+            </div>
+        </div>
+    </div>
+    
+  </body>
+
+</html>
